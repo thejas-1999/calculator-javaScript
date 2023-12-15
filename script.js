e15ce11247d55880521e16a0fb769cfbfdb58c0f
thejas-1999 <thejask074@gmail.com>
2023-12-15 12:35:20 +0530

    ok delete

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 let string = "";
-let buttons = document.querySelectorAll('.button');
-let inputField = document.querySelector('input');
+let buttons = document.querySelectorAll(".button");
+let inputField = document.querySelector("input");
 
 Array.from(buttons).forEach((button) => {
-  button.addEventListener('click', (e) => {
-    if (e.target.innerHTML === '=') {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "=") {
       string = eval(string);
       inputField.value = string;
-    }
-    else if (e.target.innerHTML === 'C') {
+    } else if (e.target.innerHTML === "C") {
       string = "";
       inputField.value = string;
-    } 
-    else {
+      console.log("ok");
+    } else {
       console.log(e.target);
       string = string + e.target.innerHTML;
       inputField.value = string;
